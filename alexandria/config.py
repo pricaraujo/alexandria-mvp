a61d360fa3f516da416ded98d0e188f7d90f1541
pricaraujo <pri11araujo41@gmail.com>
2026-07-23 19:53:53 -0300

    change dabase config

--- alexandria/config.py
+++ alexandria/config.py
@@ -5,11 +5,14 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
-    SECRET_KEY = "alexandria-mvp"
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "alexandria-mvp"
+    )
 
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" +
-        os.path.join(BASE_DIR, "alexandria.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "alexandria.db")
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
